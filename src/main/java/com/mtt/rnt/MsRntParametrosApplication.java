6004f8001a985f5214444b1709b98956725b3704
cesarOsses <cesar.osses@catena.cl>
2024-02-12 17:07:47 -0300

    inicializar

---
+++ src/main/java/com/mtt/rnt/MsRntParametrosApplication.java
@@ -0,0 +1,13 @@
+package com.mtt.rnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRntParametrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRntParametrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtt/rnt/MsRntParametrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtt.rnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRntParametrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
