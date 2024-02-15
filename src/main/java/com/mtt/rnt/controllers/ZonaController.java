eb75c19f519d7069517350f3deb93aa4b7f4d851
Rodrigo Salinas <rodrigo.salinas@catena.cl>
2024-02-15 10:52:24 -0300

    ZoneController

--- src/main/java/com/mtt/rnt/controllers/ZonaController.java
+++ src/main/java/com/mtt/rnt/controllers/ZonaController.java
@@ -1,4 +1,28 @@
 package com.mtt.rnt.controllers;
 
+import com.mtt.rnt.entities.Zona;
+import com.mtt.rnt.services.ZonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/zona", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ZonaController {
+
+    @Autowired
+    ZonaService zonaService;
+
+    @GetMapping("/regiones-ids")
+    public ResponseEntity<List<String>> getRegionesIdsFromZonas(@RequestBody List<Zona> zonas) {
+        List<String> regionesIds = zonaService.getRegionesIdsFromZonas(zonas);
+        return ResponseEntity.ok(regionesIds);
+    }
+
 }
--- src/main/java/com/mtt/rnt/services/ZonaService.java
+++ src/main/java/com/mtt/rnt/services/ZonaService.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 public interface ZonaService {
 
+    /**
+     * Devuelve los ids de las regiones correspondientes a las zonas pasadas como parámetro
+     *
+     * @param zonas Lista de zonas a consultar
+     * @return List<String> Lista de ids de regiones asociadas a las zonas
+     */
     List<String> getRegionesIdsFromZonas(List<Zona> zonas);
 
 }
