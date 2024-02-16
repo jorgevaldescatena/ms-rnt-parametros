5c1b9c3aa6885e89958c8517122756d1bbb9576a
Rodrigo Salinas <rodrigo.salinas@catena.cl>
2024-02-16 12:36:08 -0300

    getAllZL

---
+++ src/main/java/com/mtt/rnt/services/ZonaLocalidadService.java
@@ -0,0 +1,11 @@
+package com.mtt.rnt.services;
+
+import com.mtt.rnt.entities.ZonaLocalidad;
+
+import java.util.List;
+
+public interface ZonaLocalidadService {
+
+    List<ZonaLocalidad> getAllZonaLocalidad();
+
+}
---
+++ src/main/java/com/mtt/rnt/servicesImpl/ZonaLocalidadServiceImpl.java
@@ -0,0 +1,22 @@
+package com.mtt.rnt.servicesImpl;
+
+import com.mtt.rnt.entities.ZonaLocalidad;
+import com.mtt.rnt.repository.ZonaLocalidadRepository;
+import com.mtt.rnt.services.ZonaLocalidadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ZonaLocalidadServiceImpl implements ZonaLocalidadService {
+
+    @Autowired
+    ZonaLocalidadRepository zonaLocalidadRepository;
+
+    @Override
+    public List<ZonaLocalidad> getAllZonaLocalidad() {
+        return zonaLocalidadRepository.findAll();
+    }
+    
+}
