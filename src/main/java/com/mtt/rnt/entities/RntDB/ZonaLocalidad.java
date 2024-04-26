cc4eab0ab03bf0ccd88fa9f25c1fca8f774dd804
Rodrigo Salinas <rodrigo.salinas@catena.cl>
2024-04-26 01:10:22 -0400

    fix

--- src/main/java/com/mtt/rnt/entities/RntDB/ZonaLocalidad.java
+++ src/main/java/com/mtt/rnt/entities/RntDB/ZonaLocalidad.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "RNT_ZONA_LOCALIDAD")
+@Table(name = "RNT_ZONA_LOCALIDAD", schema = "NULLID")
 public class ZonaLocalidad {
 
     @Id
--- src/main/java/com/mtt/rnt/servicesImpl/ZonaServiceImpl.java
+++ src/main/java/com/mtt/rnt/servicesImpl/ZonaServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,6 +32,7 @@ public class ZonaServiceImpl implements ZonaService {
     ZonaLocalidadRepository zonaLocalidadRepository;
 
     @Override
+    @Transactional(value = "rntTransactionManager", readOnly = true)
     public Page<ZonaDTO> getAllZonas(Pageable pageable) {
         return zonaRepository.findAll(pageable)
                 .map(this::convertToDto);
